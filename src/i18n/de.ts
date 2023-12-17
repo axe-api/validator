7715f6c2c27f0ab2c28dc5b43ae61cd03998a6cc
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-12-17 20:17:32 +0100

    Fixed more translations

---
+++ src/i18n/de.ts
@@ -0,0 +1,34 @@
+import { Translation } from "../Types";
+
+const translations: Translation = {
+  accepted: "Das Feld muss akzeptiert werden.",
+  after: "Das Feld muss nach {0} liegen.",
+  after_or_equal: "Das Feld muss gleich oder nach {0} liegen.",
+  alpha: "Das Feld darf nur alphabetische Zeichen enthalten.",
+  alpha_dash:
+    "Das Feld darf nur alphanumerische Zeichen sowie Bindestriche und Unterstriche enthalten.",
+  alpha_num: "Das Feld muss alphanumerisch sein.",
+  array: "Das Feld muss ein Array sein.",
+  before: "Das Feld muss vor {0} liegen.",
+  before_or_equal: "Das Feld muss gleich oder vor {0} liegen.",
+  between: "Das Feld muss zwischen {0} und {1} liegen.",
+  boolean: "Das Feld muss einen booleschen Wert haben.",
+  confirmed: "Die Bestätigung stimmt nicht überein.",
+  email: "Das Feld muss eine E-Mail sein.",
+  date: "Das Feld hat kein gültiges Datumsformat.",
+  digits: "Das Feld muss {0} Ziffern haben.",
+  digits_between: "Das Feld muss zwischen {0} und {1} Ziffern haben.",
+  in: "Die ausgewählte Option ist ungültig.",
+  integer: "Das Feld muss eine ganze Zahl sein.",
+  hex: "Das Feld sollte das Hexadezimalformat haben",
+  min: "Das Feld muss mindestens {0} sein.",
+  max: "Das Feld darf nicht größer als {0} sein.",
+  not_in: "Die ausgewählte Option ist ungültig.",
+  numeric: "Das Feld muss eine Zahl sein.",
+  required: "Das Feld ist erforderlich.",
+  size: "Die Größe des Feldes muss {0} sein.",
+  string: "Das Feld muss eine Zeichenkette sein.",
+  url: "Das Feld muss eine URL sein.",
+};
+
+export default translations;
