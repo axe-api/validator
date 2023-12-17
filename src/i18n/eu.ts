864d2d3bb92a0ed39034607e693a0d43cd056387
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-12-17 21:00:44 +0100

    Added more translations

---
+++ src/i18n/eu.ts
@@ -0,0 +1,34 @@
+import { Translation } from "../Types";
+
+const translations: Translation = {
+  accepted: "Kanpoko eremua onartu behar da.",
+  after: "Eremua {0} ostean egon behar da.",
+  after_or_equal: "Eremua {0} berdina edo ostean egon behar da.",
+  alpha: "Eremuak alfabetako karaktereak izan behar ditu soilik.",
+  alpha_dash:
+    "Eremuak alfanumeriko karaktereak, marrak eta azpimarrak soilik izan ditzake.",
+  alpha_num: "Eremuak alfanumerikoa izan behar da.",
+  array: "Eremuak bilduma izan behar da.",
+  before: "Eremua {0} aurretik egon behar da.",
+  before_or_equal: "Eremua {0} berdina edo aurretik egon behar da.",
+  between: "Eremua {0} eta {1} artean egon behar da.",
+  boolean: "Eremua boolean balioa izan behar da.",
+  confirmed: "Eremuaren baieztapena ez dator bat.",
+  email: "Eremuak emaila izan behar du.",
+  date: "Eremua ez da data formatu baliogarria.",
+  digits: "Eremuak {0} digitu izan behar ditu.",
+  digits_between: "Eremuak {0} eta {1} digitu artean izan behar ditu.",
+  in: "Hautatutako eremua ez da baliozkoa.",
+  integer: "Eremua zenbaki osoa izan behar da.",
+  hex: "Eremuak exadecimal formatua izan behar du",
+  min: "Eremua gutxienez {0} izan behar da.",
+  max: "Eremua ezin da {0} baino handiagoa izan.",
+  not_in: "Hautatutako eremua ez da baliozkoa.",
+  numeric: "Eremua zenbaki izan behar da.",
+  required: "Eremuak derrigorrezkoa da.",
+  size: "Eremuaren tamaina {0} izan behar da.",
+  string: "Eremua karaktere-katea izan behar da.",
+  url: "Eremua URL izan behar da.",
+};
+
+export default translations;
