e1a5c5834d7a1ffe34baac03f45c6282ddaf496b
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-12-22 21:25:59 +0100

    Fixed linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,5 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
   },
-  ignorePatterns: ["dist", "node_modules", "node-tester"],
+  ignorePatterns: ["dist", "node_modules", "tests/consumers"],
 };
