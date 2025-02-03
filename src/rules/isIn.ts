3a84dc485dc514f7180b0e42b296352be33b19c0
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2025-02-03 18:06:42 +0100

    Fixed #70

--- src/rules/isIn.ts
+++ src/rules/isIn.ts
@@ -1,5 +1,15 @@
-export default (value: any, options: any[] | string): boolean => {
-  const list: any[] = Array.isArray(options) ? options : options.split(",");
+export default (value: any, ...args: any[]): boolean => {
+  // Let's check the last item is IContext
+  const lastItem: any = args.at(-1);
+  if (lastItem.definition && lastItem.field) {
+    args.pop();
+  }
+
+  const [options] = args;
+
+  const list: any[] = Array.isArray(options)
+    ? options
+    : (options as string).split(",");
   const listAsString = list.map((item) => String(item).trim());
 
   if (Array.isArray(value)) {
